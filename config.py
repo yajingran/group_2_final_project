1b32d330fb299458c6678af86e3502a346496699
KL <kassielu@gmail.com>
2020-11-13 19:54:26 -0500

    Uploaded all the updated database objects for ames data set

---
+++ config.py
@@ -0,0 +1,4 @@
+jdbc_url="jdbc:postgresql://dataviz.ce3jlxk8s6lq.us-east-2.rds.amazonaws.com:5432/house_prices_db"
+config = {"user":"postgres", 
+          "password": "Data2020Viz!", 
+          "driver":"org.postgresql.Driver"}
