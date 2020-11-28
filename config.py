d41ab7419d5188b8b303d43ba08c1d94a9bf52e9
Kassie Lu <50142434+kassielu@users.noreply.github.com>
2020-11-28 12:06:15 -0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-PGHOST = 'dataviz.ce3jlxk8s6lq.us-east-2.rds.amazonaws.com'
-PGDATABASE = 'house_prices_db'
-PGUSER = 'postgres'
-PGPASSWORD = 'Proj1998Ebony!'
-
-jdbc_url="jdbc:postgresql://dataviz.ce3jlxk8s6lq.us-east-2.rds.amazonaws.com:5432/house_prices_db"
-config = {"user":"postgres", 
-          "password": "Data2020Viz!", 
-          "driver":"org.postgresql.Driver"}
