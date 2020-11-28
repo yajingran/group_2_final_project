5cd8c47d95599cd922273350507ae1d49c631eca
KL <kassielu@gmail.com>
2020-11-28 12:05:44 -0500

    Upload Tableau changes

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 PGHOST = 'dataviz.ce3jlxk8s6lq.us-east-2.rds.amazonaws.com'
 PGDATABASE = 'house_prices_db'
 PGUSER = 'postgres'
-PGPASSWORD = 'Data2020Viz!'
+PGPASSWORD = 'Proj1998Ebony!'
 
 jdbc_url="jdbc:postgresql://dataviz.ce3jlxk8s6lq.us-east-2.rds.amazonaws.com:5432/house_prices_db"
 config = {"user":"postgres", 
