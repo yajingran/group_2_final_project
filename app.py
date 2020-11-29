bbe0ab87a7c22e618c27ae040593e2bc84fb0954
yajingran <yajingran@yahoo.ca>
2020-11-29 15:30:32 -0500

    ml

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import request, Flask, render_template
 from flask_pymongo import PyMongo
 from joblib import dump, load
 import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.linear_model import LinearRegression
 clf = load('mlr_model.joblib')
  
 # from flask import Flask
 app = Flask(__name__)
  
 # model = pickle.load(open('model.pkl', 'rb'))
-col=['neighborhood','total_area','overallqual','garagecars','fullbath','yearbuilt','yearremodadd','yrsold']
+col=['neighborhood','total_area','overallqual','garagecars','fullbath','yearbuilt','yearremodadd']
  
 @app.route("/")
 def index():
@@ -19,10 +21,7 @@ def index():
 def predict():
     int_features = [int(x) for x in request.form.values()]
     final = [np.array(int_features, dtype=float)]
-    scaler=preprocessing.StandardScaler().fit(final)
-    input_data_scaled=scaler.transform(final)
-    y_test_pred = mlr_model.predict(final)
-    prediction = np.exp(y_test_pred)
+    prediction=mlr_model.predict(final)
     output = round(prediction[0],2)
  
     return render_template('index.html', pred=123)
